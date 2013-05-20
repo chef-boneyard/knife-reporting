5439abaff02f31804a1da81f7b1240eaa4b8c71c
Matthew Peck <matthew@opscode.com>
2013-05-20 12:27:11 -0400

    Just show important information in our full history list

--- lib/chef/knife/run_list.rb
+++ lib/chef/knife/run_list.rb
@@ -9,7 +9,15 @@ class Chef
         rest = Chef::REST.new(Chef::Config[:chef_server_url])
 
         runs = rest.get_rest("reports/org/runs", false, {'X-Ops-Reporting-Protocol-Version' => PROTOCOL_VERSION} )
-        output(runs)
+
+        run_history = runs["run_history"].map do |run|
+          { :run_id => run["run_id"],
+            :node_name => run["node_name"],
+            :status => run["status"],
+            :start_time => run["start_time"] }
+        end
+
+        output(run_history)
       end
     end
   end
