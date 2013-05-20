c24507035d7703754dfa60fb8cdc58b9def804e3
Matthew Peck <matthew@opscode.com>
2013-05-20 16:57:52 -0400

    runs list now returns runs from the org or a single node

--- lib/chef/knife/run_history.rb
+++ lib/chef/knife/run_history.rb
@@ -1,24 +1,51 @@
 class Chef
   class Knife
-    class RunHistory < Chef::Knife
-      banner "knife run history"
+    class RunsList < Chef::Knife
+      banner "knife runs list [<node name>]"
 
       PROTOCOL_VERSION = '0.1.0'
+      HEADERS = {'X-Ops-Reporting-Protocol-Version' => PROTOCOL_VERSION}
 
       def run
-        rest = Chef::REST.new(Chef::Config[:chef_server_url])
+        @rest = Chef::REST.new(Chef::Config[:chef_server_url])
 
-        runs = rest.get_rest("reports/org/runs", false, {'X-Ops-Reporting-Protocol-Version' => PROTOCOL_VERSION} )
+        node_name = name_args[0]
 
-        run_history = runs["run_history"].map do |run|
+        if node_name
+          runs = node_history(node_name)
+        else
+          runs = org_history
+        end
+
+
+        output(runs)
+      end
+
+      private
+
+      def org_history
+        runs = @rest.get_rest("reports/org/runs", false,  HEADERS)
+
+        runs["run_history"].map do |run|
           { :run_id => run["run_id"],
             :node_name => run["node_name"],
             :status => run["status"],
             :start_time => run["start_time"] }
         end
-
-        output(run_history)
       end
+
+      def node_history(node_name)
+        runs = @rest.get_rest( "reports/nodes/#{node_name}/runs", false, HEADERS )
+
+        runs["run_history"].map do |run|
+          # For some reason we don't return a run_id so we have to strip it from the uri
+          { :run_id => run["uri"].split("/").last,
+            :node_name => run["node_name"],
+            :status => run["status"],
+            :start_time => run["start_time"] }
+        end
+      end
+
     end
   end
 end
