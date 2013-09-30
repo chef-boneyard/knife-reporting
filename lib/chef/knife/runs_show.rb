e5fde64c24187cee4af2e42a1c529c94ffcd911c
mmzyk <mmzyk@opscode.com>
2013-09-30 17:11:40 -0400

    Add option for user to specify start/end times and sanity checks
    Sanity check includes user not requesting more than 3 months
    of data at a time

--- lib/chef/knife/runs_show.rb
+++ lib/chef/knife/runs_show.rb
@@ -1,29 +1,41 @@
 class Chef
   class Knife
     class RunsShow < Chef::Knife
+
+      deps do
+        # While Ruby automatically includes some data & time functions in the
+        # base class, more advanced data & time functions still required the
+        # modules be loaded.
+        require 'time'
+        require 'date'
+      end
+
       banner "knife runs show <node name> [<run id>]"
 
       PROTOCOL_VERSION = '0.1.0'
       HEADERS = {'X-Ops-Reporting-Protocol-Version' => PROTOCOL_VERSION}
       SECONDS_IN_24HOURS = 86400
-
-      # Requiring these as unix timestamps is not the best user friendly format
-      # but it is what the server expects.
-      # Options to supply the time in other formats should be provided
-      # and knife should convert it.
-      # Do we have libs to do this already for other plugins?
+      # Approximate, b/c of course the length of a month can vary
+      SECONDS_IN_3MONTHS = 7889230
 
       option :start_time,
-        :long => '--start_time UNIX_TIMESTAMP',
-        :short => '-s UNIX_TIMESTAMP',
+        :long => '--start_time MM-DD-YYYY',
+        :short => '-s MM-DD-YYYY',
         :required => false,
-        :description => 'Find runs with a start time great than or equal to the time provided'
+        :description => 'Find runs with a start time great than or equal to the date provided. If the -u option is provided unix timestamps can be given instead.'
 
       option :end_time,
-        :long => '--end_time UNIX_TIMESTAMP',
-        :short => '-e UNIX_TIMESTSAMP',
+        :long => '--end_time MM-DD-YYYY',
+        :short => '-e MM-DD-YYYY',
         :required => false,
-        :description => 'Find runs with an end time less than or equal to the time provided'
+        :description => 'Find runs with an end time less than or equal to the date provided. If the -u option is provided unix timestamps can be given instead.'
+
+      option :unix_timestamps,
+        :long => '--unix_timestamps',
+        :short => '-u',
+        :required => false,
+        :boolean => true,
+        :description => 'Indicates start and end times are given as unix time stamps and not date formats.'
 
       def run
         rest = Chef::REST.new(Chef::Config[:chef_server_url])
@@ -40,8 +52,9 @@ class Chef
           # If run_id is nil then we are hitting the nodes endpoint and requesting info about multiple runs
           # start and end time options are relevant here
 
-          verify_time_args()
+          check_start_and_end_times_provided()
           start_time, end_time = apply_time_args()
+          check_3month_window(start_time, end_time)
           query_string = "reports/nodes/#{node_name}/runs?from=#{start_time}&until=#{end_time}"
         else
           # If run_id is not nil, then we want a specific run and start and end times are irrelevent
@@ -53,7 +66,27 @@ class Chef
         output(runs)
       end
 
-      def verify_time_args()
+
+      # Helper Functions
+
+      def apply_time_args()
+        if config[:start_time] && config[:end_time]
+          start_time, end_time = convert_to_unix_timestamps()
+        else
+          start_time, end_time = last_24hours_time_window()
+        end
+
+        return start_time, end_time
+      end
+
+      def last_24hours_time_window()
+        # Time is calculated as a unix timestamp
+        end_time = Time.now.to_i
+        start_time = end_time - SECONDS_IN_24HOURS
+        return start_time, end_time
+      end
+
+      def check_start_and_end_time_provided()
         if config[:start_time] && !config[:end_time]
           ui.error("The start_time option was provided, but the end_time option was not. If one is provided, the other is required.")
           exit 1
@@ -63,23 +96,30 @@ class Chef
         end
       end
 
-      def apply_time_args()
-        if config[:start_time] && config[:end_time]
-          start_time = config[:start_time]
-          end_time = config[:end_time]
+      def convert_to_unix_timestamps()
+        if config[:unix_timestamps]
+          start_time = config[:start_time].to_i
+          end_time = config[:end_time].to_i
         else
-          start_time, end_time = last_24hours_time_window()
+          # Take user supplied input, assumes it is in a valid date format,
+          # convert to a time object, and then convert to a unix timestamp
+          # If the format isn't valid Time will throw an error
+          # The options suggest MM-DD-YYYY b/c that is consistent with the
+          # reporting UI, but it isn't actually required for this to work
+          # Question: will this work on windows?
+          start_time = Time.parse(config[:start_time]).to_i
+          end_time = Time.parse(config[:end_time]).to_i
         end
 
         return start_time, end_time
       end
 
-
-      def last_24hours_time_window()
-        # Time is calculated as a unix timestamp
-        end_time = Time.now.to_i
-        start_time = end_time - SECONDS_IN_24HOURS
-        return start_time, end_time
+      def check_3month_window(start_time, end_time)
+        # start_time and end_time are unix timestamps
+        if (end_time - start_time) > SECONDS_IN_3MONTHS
+          ui.error("Requesting information for more than three months at a time is disallowed. Please try a smaller timeframe")
+          exit 1
+        end
       end
 
     end
