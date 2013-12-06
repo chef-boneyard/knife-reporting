84bc6e7b0891c3db9ecab6f278a4ecf21042d357
PrajaktaPurohit <prajakta@opscode.com>
2013-12-06 01:23:53 -0800

    Deleting unneeded options

--- lib/chef/knife/runs_show.rb
+++ lib/chef/knife/runs_show.rb
@@ -18,25 +18,6 @@ class Chef
       PROTOCOL_VERSION = '0.1.0'
       HEADERS = {'X-Ops-Reporting-Protocol-Version' => PROTOCOL_VERSION}
 
-      option :start_time,
-        :long => '--starttime MM-DD-YYYY',
-        :short => '-s MM-DD-YYYY',
-        :required => false,
-        :description => 'Find runs with a start time great than or equal to the date provided. If the -u option is provided unix timestamps can be given instead.'
-
-      option :end_time,
-        :long => '--endtime MM-DD-YYYY',
-        :short => '-e MM-DD-YYYY',
-        :required => false,
-        :description => 'Find runs with an end time less than or equal to the date provided. If the -u option is provided unix timestamps can be given instead.'
-
-      option :unix_timestamps,
-        :long => '--unixtimestamps',
-        :short => '-u',
-        :required => false,
-        :boolean => true,
-        :description => 'Indicates start and end times are given as unix time stamps and not date formats.'
-
       def run
         rest = Chef::REST.new(Chef::Config[:chef_server_url])
 
